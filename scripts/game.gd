dde245358f4ae8887845537d0e4ea4963c61eb78
Primitheus <Primitheus@gmail.com>
2025-02-20 11:36:23 +0000

    Updated.
    Fixed Skeleton not detecting player.
    Changed spawn method for the player object.

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,8 +4,14 @@ extends Node2D
 func _ready() -> void:
 	$World.generate_new_world()
 
-	$Player.position = $World.get_spawn_position()
-	$skeleton.position = $World.get_spawn_position()
+	#$Player.position = $World.get_spawn_position()
+	$skeleton_enemy.position = $World.get_spawn_position() + Vector2i(0, -200)
 	
-	$Player.get_node("CanvasLayer/HUD/Minimap").world_size = $World.WORLD_SIZE
-	$Player.get_node("CanvasLayer/HUD/Minimap").draw($World.foreground)
+	var player = load("res://player.tscn").instantiate()
+	var Game = get_node(".")
+	Game.add_child(player)
+	
+	player.position = $World.get_spawn_position()
+	
+	player.get_node("CanvasLayer/HUD/Minimap").world_size = $World.WORLD_SIZE
+	player.get_node("CanvasLayer/HUD/Minimap").draw($World.foreground)
--- scripts/skeleton_enemy.gd
+++ scripts/skeleton_enemy.gd
@@ -1,8 +1,5 @@
 extends "res://scripts/entity.gd"
 
-@onready var player: CharacterBody2D = $Player
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
@@ -10,24 +7,24 @@ func _ready() -> void:
 
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
-	
+	print("Entered Detection Area")
 	targeting_actor = body
 	is_following = true
-	
-	
 	
 	pass # Replace with function body.
 
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
-	
+	print("Exited Detection Area")
+
 	targeting_actor = null
 	is_following = false
-	
 	
 	pass # Replace with function body.
 
 func _physics_process(delta: float) -> void:
+	#super._physics_process(delta)  # Runs parent physics first
+
 	if is_following:
 		position += (targeting_actor.position - position)/SPEED
 		
