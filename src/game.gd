38437c68c51f9aa07d78245cdbbb9a75b914b533
EamonnER <eamonn.e.rolander@gmail.com>
2025-02-25 16:58:36 +0000

    Slightly changed background

--- src/game.gd
+++ src/game.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var world: World = $World
 @onready var camera: Camera2D = $Camera
 @onready var hud: Control = $CanvasLayer/HUD
+@onready var background: Background = $Background
 
 func _ready() -> void:
 	world.generate_new_world()
@@ -15,6 +16,7 @@ func _ready() -> void:
 	if player.is_multiplayer_authority():  # Only follow the local player
 		camera.set_target(player)
 		hud.set_target(player)
+		background.set_target(player)
 	
 	hud.get_node("Minimap").world_size = world.WORLD_SIZE
 	hud.get_node("Minimap").draw(world.foreground)
