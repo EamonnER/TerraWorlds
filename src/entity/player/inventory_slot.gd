1876112e6a2ee2cfea072a0be004f44ef9228406
EamonnER <eamonn.e.rolander@gmail.com>
2025-02-27 13:41:09 +0000

    Added logic for inventory slot

---
+++ src/entity/player/inventory_slot.gd
@@ -0,0 +1,29 @@
+extends Node2D
+class_name InventorySlot
+
+var item: InventoryItem  # This is essentually a fancy Sprite2D
+var quantity: int
+
+func set_item(new_item: InventoryItem, new_quantity: int) -> void:
+	item = new_item
+	quantity = new_quantity if new_quantity > 0 else null
+
+func increment_quantity(amount: int) -> void:
+	quantity += amount
+
+func decrement_quantity(amount: int) -> void:
+	quantity -= amount
+	if quantity < 0:
+		item = null
+		quantity = 0
+
+func get_item() -> InventoryItem:
+	return item
+	
+func get_quantity() -> int:
+	return quantity
+
+func set_quantity(new_quantity: int) -> void:
+	quantity = new_quantity
+	if quantity < 0:
+		quantity = 0
