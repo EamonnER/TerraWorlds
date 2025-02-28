40e462da03d08e530909755231d03b458434d3a7
EamonnER <eamonn.e.rolander@gmail.com>
2025-02-28 16:07:06 +0000

    Added text for item quantity

--- src/inventory/inventory_slot.gd
+++ src/inventory/inventory_slot.gd
@@ -3,6 +3,8 @@ class_name InventorySlot
 
 @export var quantity: int = 0
 
+@onready var quantity_label: Label = $Quantity
+
 func set_item(new_item: Item, new_quantity: int) -> void:
 	var current_item = get_item()
 	if current_item and new_item.id == current_item.id:
@@ -13,18 +15,22 @@ func set_item(new_item: Item, new_quantity: int) -> void:
 	inventory_item.set_position(get_size()/2)
 	inventory_item.set_scale(Vector2(0.7, 0.7))
 	quantity = max(new_quantity, 0)
+	_update_label()
 
 func increment_quantity(amount: int) -> void:
 	quantity += amount
+	_update_label()
 
 func decrement_quantity(amount: int) -> void:
 	quantity -= amount
 	if quantity < 0:
 		delete_item()
+	_update_label()
 
 func get_item() -> Item:
 	for child in get_children():
-		return child.item
+		if child is InventoryItem:
+			return child.item
 	return null
 	
 func get_quantity() -> int:
@@ -34,14 +40,23 @@ func set_quantity(new_quantity: int) -> void:
 	quantity = new_quantity
 	if quantity < 0:
 		quantity = 0
+	_update_label()
 
 func delete_item() -> void:
 	for child in get_children():
-		remove_child(child)
-		child.queue_free()
-		quantity = 0
+		if child is InventoryItem:
+			remove_child(child)
+			child.queue_free()
+			quantity = 0
+	_update_label()
 
 func pop_item() -> Item:
 	var item = get_item()
 	delete_item()
 	return item
+
+func _update_label() -> void:
+	if quantity > 0:
+		quantity_label.set_text(str(quantity))
+		return
+	quantity_label.set_text("")
