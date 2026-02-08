0364fa3e8e02a21f8619cb892cca7ae64bba0a1c
EamonnER <eamonn.e.rolander@gmail.com>
2026-02-08 14:00:41 +0000

    Made servers in list selectable

--- src/menu/play_menu/saved_server_list_item.gd
+++ src/menu/play_menu/saved_server_list_item.gd
@@ -8,7 +8,7 @@ func _on_edit_server_button_toggled(toggled_on: bool) -> void:
 	$VBoxContainer/ServerDetailsContainer.visible = toggled_on
 
 func set_details(server_name: String, address: String, port: int = GlobalVariables.DEFAULT_PORT) -> void:
-	$VBoxContainer/HBoxContainer/ServerNameLabel.set_text(server_name)
+	$VBoxContainer/HBoxContainer/ServerSelectButton.set_text(server_name)
 	$VBoxContainer/ServerDetailsContainer/VBoxContainer/EditServerNameInput.set_text(server_name)
 	
 	$VBoxContainer/ServerDetailsContainer/VBoxContainer/EditServerAddressInput.set_text(address)
